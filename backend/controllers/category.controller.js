7627f10d906bd333559a49c039128288459aa3a2
Asif Hasan Plabon <ahplabon36@gmail.com>
2026-06-16 01:53:05 +0600

    Update category.controller.js

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,4 +1,4 @@
-import Category from "../models/Category.model.js";
+import Category from "../models/category.model.js";
 import slugify from "slugify"; // npm install slugify
 
 // ── Create category ──
