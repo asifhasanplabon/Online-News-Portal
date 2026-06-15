63347559e71529c4d66c93996f5b147d6f14681c
ahp007 <asif_21102016@jkkniu.edu.bd>
2026-06-16 00:43:10 +0600

    update the case sensitive

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ import newsRouter from "./routes/news.routes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7100;
 
 // ── DB + Cloudinary connect ──
 connectDB();
