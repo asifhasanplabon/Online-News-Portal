8a31b33b89e7ef8775d397ea34b71f0e8b4c317c
Asif Hasan Plabon <ahplabon36@gmail.com>
2026-06-16 01:56:52 +0600

    Update auth.middleware.js

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.model.js";
+import User from "../models/user.model.js";
 
 // ── 1. Verify Token (must be logged in) ──
 export const verifyToken = async (req, res, next) => {
